67a6280897ad3b2849ff0eed85bd7a3d64101e20
Andre Costa <fc.andre.costa@gmail.com>
2017-03-15 11:11:40 +0000

    basic colors (NO CHANGE EVER)

--- KokoEngine/Color.cs
+++ KokoEngine/Color.cs
@@ -12,8 +12,11 @@ namespace KokoEngine
         public int G;
         public int B;
 
-        public static Color Red => new Color(255, 0, 0);
         public static Color White => new Color(255, 255, 255);
+        public static Color Black => new Color(0, 0, 0);
+        public static Color Red => new Color(255, 0, 0);
+        public static Color Green => new Color(0, 255, 0);
+        public static Color Blue => new Color(0, 0, 255);
 
         public Color(int r, int g, int b)
         {
